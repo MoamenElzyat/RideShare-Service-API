05cad31b3fa134cee8efacdab5b6282f755c7ad5
YoussefShakib <youssefshakib@gmail.com>
2025-04-14 23:53:23 +0200

    first stage

---
+++ src/main/java/com/example/miniapp/models/Payment.java
@@ -0,0 +1,30 @@
+package com.example.miniapp.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "payments")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NonNull
+    private Double amount;
+
+    @NonNull
+    private String paymentMethod;
+
+    @NonNull
+    private Boolean paymentStatus;
+
+    @OneToOne
+    @JoinColumn(name = "trip_id", referencedColumnName = "id")
+    private Trip trip;
+}
